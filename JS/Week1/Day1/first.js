f98754c9734a06dec9680a0ba8aa4019c288bfae
Sk Asfar Ali <116512658+Asfar35@users.noreply.github.com>
2024-12-01 21:17:34 +0530

    Add files via upload

---
+++ JS/Week1/Day1/first.js
@@ -0,0 +1,9 @@
+console.log("Hello World!!!");
+let name = "Asfar";
+console.log("Name: " + name);
+const id = 1;
+console.log("I'd: " + id);
+
+let a=9825425943956525545548641679n;
+console.log(a);
+console.log(typeof a);
---
+++ JS/Week1/Day4/first.js
@@ -0,0 +1,19 @@
+console.log(typeof null);
+console.log(typeof 123n);
+
+console.log(10 == "10");
+console.log(10 === "10");
+
+console.log(null == undefined);
+console.log(null === undefined);
+
+console.log(null == 0);
+console.log(null < 0);
+console.log(null > 0);
+console.log(null <= 0);
+console.log(null >= 0);
+
+
+console.log(typeof NaN);
+console.log(NaN == NaN);
+console.log(isNaN(NaN));
